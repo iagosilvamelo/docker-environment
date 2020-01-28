25fd9b6c2709cdacf4a919163b6007facb08bb73
Iago Melo <iagosilvamelo@gmail.com>
2020-01-28 10:45:53 -0300

    Atualizado diretorios.

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-alias pabx-api="/usr/bin/php /home/iago/Projetos/Liga-v2/application/pabx-api/artisan"
-alias liga-api="/usr/bin/php /home/iago/Projetos/Liga-v2/application/liga-api/artisan"
-
-docker-compose up -d
