003cb30ad4ff2a32c4dd3cba31e2455ca88d247d
Iago Melo <iagosilvamelo@gmail.com>
2020-01-28 11:04:07 -0300

    Adicionado  sh de inicialização.

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+alias pabx-api="/usr/bin/php /home/iago/Projetos/DAD/Liga-v2/App/pabx-api/artisan"
+alias liga-api="/usr/bin/php /home/iago/Projetos/DAD/Liga-v2/App/liga-api/artisan"
+
+docker-compose up -d
